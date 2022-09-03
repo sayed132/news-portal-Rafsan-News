ceed862d349146c4e3b0aeb8472eb8bc201830e6
Abu Sayed <rafsansayed132@gmail.com>
2022-09-03 18:13:21 +0600

    added news section

--- script/main.js
+++ script/main.js
@@ -5,6 +5,8 @@ const newsCategory = async () =>{
     displayCategory(data.data.news_category)
 }
 
+
+
 const displayCategory = (categorys) =>{
     const getCategoryDiv = document.getElementById('navbar-items');
 
@@ -14,7 +16,7 @@ const displayCategory = (categorys) =>{
         createCategoryList.classList.add('nav-pills')
         createCategoryList.classList.add('nav-fill')
         createCategoryList.innerHTML =`
-            <li class="list-gruop nav-item"> 
+            <li class="list-gruop nav-item" onclick="loadNews(${category.category_id})"> 
             <a class="nav-link" href="#">${category.category_name}</a>  
             </li>
         `;
@@ -23,3 +25,52 @@ const displayCategory = (categorys) =>{
 }
 
 newsCategory()
+// category js ends here 
+
+// display news starts 
+const loadNews = async (newss) =>{
+    const url = `https://openapi.programming-hero.com/api/news/category/0${newss}`
+    const res = await fetch(url)
+    const data = await res.json()
+    displayNews(data.data)
+};
+
+const displayNews = (newss) =>{
+    const getNewsDiv = document.getElementById('news-section');
+    getNewsDiv.textContent = '';
+    for (const news of newss){
+        const createNewsDiv = document.createElement('div');
+        createNewsDiv.classList.add('card')
+        createNewsDiv.classList.add('mb-3')
+        createNewsDiv.innerHTML =`
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="${news.image_url}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">${news.title}</h5>
+                    <p class="card-text">${news.details}</p>
+                    <div class="d-flex align-items-center justify-content-between">
+                        <div class="d-flex justify-content-between">
+                        <img src="${news.author.img}" class=" img-name rounded-circle">
+                        <div class="repoter">
+                        <h4 class="card-text">${news.author.name}</h4>
+                        <p>${news.author.published_date}</p>
+                        </div>
+                        
+                        </div>
+                        <p class="text-muted"><i class="fa-solid fa-eye"></i>  ${news.total_view}</p>
+                        <p class="text-muted">${news.rating.number}</p>
+                        <p><i class="fa-solid fa-arrow-right"></i></p>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        
+        `;
+        getNewsDiv.appendChild(createNewsDiv)
+    }
+}
+loadNews()
